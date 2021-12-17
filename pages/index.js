f428c8f44ad97156e7ae558e9b25b98486c28c06
epthalite <fmy628.1998@icloud.com>
2021-12-17 18:10:23 +0900

    change introduce

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[I am Yumeoi Bit]</p>
+        <p>[I am Next.js]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
